990e2285d9f44b10486582d5e2237fb81573ff6c
Kagura 2718 <kagura2718@gmail.com>
2017-03-02 20:58:51 +0900

    the previous version

---
+++ total-conversion-build.meta.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @id             ingress-intel-total-conversion@jonatkins
+// @name           IITC: Ingress intel map total conversion
+// @version        0.26.0.20170108.21732
+// @namespace      https://github.com/jonatkins/ingress-intel-total-conversion
+// @updateURL      https://static.iitc.me/build/release/total-conversion-build.meta.js
+// @downloadURL    https://static.iitc.me/build/release/total-conversion-build.user.js
+// @description    [iitc-2017-01-08-021732] Total conversion for the ingress intel map.
+// @include        https://*.ingress.com/intel*
+// @include        http://*.ingress.com/intel*
+// @match          https://*.ingress.com/intel*
+// @match          http://*.ingress.com/intel*
+// @include        https://*.ingress.com/mission/*
+// @include        http://*.ingress.com/mission/*
+// @match          https://*.ingress.com/mission/*
+// @match          http://*.ingress.com/mission/*
+// @grant          none
+// ==/UserScript==
