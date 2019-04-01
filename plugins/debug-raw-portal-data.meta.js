f55ca556fe8b52629e1b56f80027ba282953d66f
Kagura 2718 <kagura2718@gmail.com>
2019-04-01 09:58:26 +0900

    import upstream

---
+++ plugins/debug-raw-portal-data.meta.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @id             iitc-plugin-raw-portal-data
+// @name           IITC plugin: Debug: Raw portal JSON data
+// @category       Debug
+// @version        0.2.4.20170108.21732
+// @namespace      rawdata
+// @updateURL      https://static.iitc.me/build/release/plugins/debug-raw-portal-data.meta.js
+// @downloadURL    https://static.iitc.me/build/release/plugins/debug-raw-portal-data.user.js
+// @description    [iitc-2017-01-08-021732] Developer debugging aid: Add a link to the portal details to show the raw data of a portal.
+// @include        https://*.ingress.com/intel*
+// @include        http://*.ingress.com/intel*
+// @match          https://*.ingress.com/intel*
+// @match          http://*.ingress.com/intel*
+// @include        https://*.ingress.com/mission/*
+// @include        http://*.ingress.com/mission/*
+// @match          https://*.ingress.com/mission/*
+// @match          http://*.ingress.com/mission/*
+// @grant          none
+// ==/UserScript==
